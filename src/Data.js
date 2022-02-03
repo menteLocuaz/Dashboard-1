b6a9fb13dc6ec4ee75ea8a5864979e28e1e5631d
menteLocuaz <emersondurate.45@gmail.com>
2022-02-03 12:29:40 -0500

    update

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export const Userdata = [
+  {
+    name: "Enero",
+    "Usuarios Activos": 4000,
+  },
+  {
+    name: "Febrero",
+    "Usuarios Activos": 3000,
+  },
+  {
+    name: "Marzo",
+    "Usuarios Activos": 2000,
+  },
+  {
+    name: "Abril",
+    "Usuarios Activos": 2780,
+  },
+  {
+    name: "Mayo",
+    "Usuarios Activos": 1890,
+  },
+  {
+    name: "Junio",
+    "Usuarios Activos": 2390,
+  },
+  {
+    name: "Julio",
+    "Usuarios Activos": 3490,
+  },
+];
